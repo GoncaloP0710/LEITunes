b799628f30f901e8946aced46a693ed5d02f8128
Goncalo Pinto <fc58178@alunos.fc.ul.pt>
2023-03-28 20:04:03 +0100

    MusicLibraryController atualizado

--- src/domain/facade/MusicLibraryController.java
+++ src/domain/facade/MusicLibraryController.java
@@ -1,10 +1,17 @@
 package domain.facade;
 
 import java.util.Optional;
-import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
+import com.mpatric.mp3agic.ID3v1;
+import com.mpatric.mp3agic.ID3v2;
+import com.mpatric.mp3agic.Mp3File;
+
 import domain.core.MusicLibrary;
+import domain.core.Song;
+import domain.core.SongMetaInfo;
 
 public class MusicLibraryController {
 	
@@ -18,14 +25,38 @@ public class MusicLibraryController {
 		return library.size();
 	}
 	
-	//------------------------------------------------
 	public void addSong(String filename) {
-
-		
-		Song song = new Song(, filename);
-		library.add(song);
+		try {
+			Mp3File mp3 = new Mp3File(filename);
+			if (mp3.hasId3v1Tag()) {
+				ID3v1 tag = mp3.getId3v1Tag();
+		        String title = tag.getTitle();
+		        String artist = tag.getArtist();
+		        String album = tag.getAlbum();
+		        String genre = tag.getGenreDescription();
+		        List<String> artists = new ArrayList<String>();
+		        artists.add(artist);
+		        SongMetaInfo songInfo = new SongMetaInfo(title, genre, artists, album);
+		        Song song = new Song(songInfo, filename);
+		        library.add(song);
+			} else if (mp3.hasId3v2Tag()) {
+				ID3v2 tag = mp3.getId3v2Tag();
+				String title = tag.getTitle();
+		        String artist = tag.getArtist();
+		        String album = tag.getAlbum();
+		        String genre = tag.getGenreDescription();
+		        List<String> artists = new ArrayList<String>();
+		        artists.add(artist);
+		        SongMetaInfo songInfo = new SongMetaInfo(title, genre, artists, album);
+		        Song song = new Song(songInfo, filename);
+		        library.add(song);
+			}
+		} catch (IOException e) {
+			System.err.println("Error reading MP3 file: " + e.getMessage());
+		} catch (Exception e) {
+			System.err.println("Error reading MP3 file: " + e.getMessage());
+		}
 	}
-	//------------------------------------------------
 	
 	public void selectSong(int i) {
 		if(i < numberOfSongs() && i >= 0) {
